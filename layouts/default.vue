2a465cdba4af1b4576dfdc6fec2e2c1d3affe5d4
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-03-17 22:28:01 +0100

    :sparkles: Initialise la page d'attestation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="content">
     <nuxt />
   </div>
 </template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -22,8 +22,7 @@ export default {
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: ['~/css/main.css'],
   /*
   ** Plugins to load before mounting the App
   */
